990af929e1b47a0f2e8477c411a55effe3f1293b
Sunkissed z <alextunder2561@gmail.com>
2024-05-30 21:33:21 +0300

    my first commit

---
+++ src/pages/backoffice/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BackOffice from '../../components/BackOffice';
+
+function Home() {
+  return (
+    <BackOffice>
+    </BackOffice>
+  )
+}
+
+export default Home;
